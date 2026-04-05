ac77dececc78dd663eb856457851d2d4de86ed57
Nadia Rahmah <nadiarahmah888@gmail.com>
2026-04-05 16:25:10 +0800

    Delete Posttest-3 directory

--- Posttest-3/src/Kelola/PaketHemat.java
+++
@@ -1,7 +0,0 @@
-package Kelola;
-
-public class PaketHemat extends SnackBox{
-    public PaketHemat(String id, String isiSnack, double harga) {
-        super(id, "Paket Hemat", isiSnack, harga);
-    }
-}
--- Posttest-3/src/Kelola/PaketKenyang.java
+++
@@ -1,7 +0,0 @@
-package Kelola;
-
-public class PaketKenyang extends SnackBox {
-    public PaketKenyang(String id, String isiSnack, double harga) {
-        super(id, "Paket kenyang", isiSnack, harga);
-    }
-}
