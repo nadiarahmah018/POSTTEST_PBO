dd4cb0f599125c7ca439dd8bcde131a93671f2a9
Nadia Rahmah <nadiarahmah888@gmail.com>
2026-04-05 16:28:27 +0800

    up posttest 3

---
+++ Posttest3/src/Kelola/SnackBox.java
@@ -0,0 +1,52 @@
+package Kelola;
+
+public class SnackBox {
+    private String id;
+    private String namaPaket;
+    private String isiSnack;
+    private double harga;
+    protected String status = "Tersedia";
+
+    public SnackBox( String id, String namaPaket, String isiSnack, double harga){
+        this.id = id;
+        this.namaPaket = namaPaket;
+        this.isiSnack = isiSnack;
+        this.harga = harga;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getNamaPaket(){
+        return namaPaket;
+    }
+
+    public void setNamaPaket(String namaPaket){
+        this.namaPaket = namaPaket;
+    }
+
+    public String getIsiSnack(){
+        return isiSnack;
+    }
+
+    public void setIsiSnack(String isiSnack){
+        this.isiSnack = isiSnack;
+    }
+
+    public double getHarga(){
+        return harga;
+    }
+
+    public void setHarga(double harga){
+        this.harga = harga;
+    }
+
+    public String getStatus(){
+        return status;
+    }
+
+    public void setStatus(String status){
+        this.status = status;
+    }
+}
